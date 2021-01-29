a35eda1539f981878a4eca5344cbc73370556177
Jelle <zuiderveldjelle@gmail.com>
2021-01-29 12:38:50 +0100

    start

---
+++ components/ListImage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <section class="grid grid-cols-3 gap-4 w-1/6 mx-auto">
+    <button class="rounded-md bg-orange-400 h-10 w-10" id="btn1" v-on:click="img = '1-200x100.jpg'"></button>
+    <button class="rounded-md bg-orange-500 h-10 w-10" id="btn2" v-on:click="img = '2-200x100.jpg'"></button>
+    <button class="rounded-md bg-orange-900 h-10 w-10" id="btn3" v-on:click="img = '3-200x100.jpg'"></button>
+  </section>
+    <div class="container mx-auto py-12">
+      <img v-bind:src="require('~/assets/' +img)" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListImage',
+  data() {
+    return {
+      img: '1-200x100.jpg'
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="container mx-auto">
+      <h1 class="title">
+        Carousel
+      </h1>
+    </div>
+
+    <div>
+    <ListImage></ListImage>
+    </div>
+  </div>
+</template>
+
+<script>
+import ListImage from "@/components/ListImage";
+
+export default {
+  components: ListImage
+}
+
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  display: flex;
+  justify-content:center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+</style>
